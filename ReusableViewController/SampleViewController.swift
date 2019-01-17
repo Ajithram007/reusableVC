ba6c8d5febdc6b65b2989cf39a69d31d8b0ec434
Ajithram007 <ajithramhacks@gmail.com>
2019-01-17 12:58:06 +0530

    Update SampleViewController.swift

--- ReusableViewController/SampleViewController.swift
+++ ReusableViewController/SampleViewController.swift
@@ -8,7 +8,7 @@
 
 import  Foundation
 open class SampleViewController: NSObject {
-    open class func test(msg:String){
+    class func test(msg:String){
         print(msg + "hello from mars!")
     }
 }
