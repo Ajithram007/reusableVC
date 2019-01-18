cd294ea0625ad58bf4e5975deea75383c0fa7e43
Ajithram M <>
2019-01-18 11:46:08 +0530

    SignIn

---
+++ ReusableViewController/SignInView.swift
@@ -0,0 +1,21 @@
+//
+//  SignIn.swift
+//  ReusableViewController
+//
+//  Created by Ajithram M on 18/01/19.
+//  Copyright © 2019 Ajithram. All rights reserved.
+//
+
+import UIKit
+
+open class SignInView: UIView {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+     @IBOutlet weak var submitButton: UIButton!
+     @IBOutlet weak var passwordTextField: UITextField!
+    
+    open override func draw(_ rect: CGRect) {
+        
+    }
+
+}
